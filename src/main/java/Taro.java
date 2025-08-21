f24bf9fabde463b24aebc199c5d708e6ede278eb
stefuiii <jianglh0425@outlook.com>
2025-08-22 01:16:55 +0800

    Add support for echo

--- src/main/java/Taro.java
+++ src/main/java/Taro.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
 public class Taro {
+    private static final String Logo =
+            "  /^ ^\\\n" +
+                    " / 0 0 \\\n" +
+                    " V\\ ^ /V\n" +
+                    "  / - \\\n" +
+                    " |    \\\n" +
+                    " || (__V";
+    private static final String LINE =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
+        //Input from user
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Taro.");
+        System.out.println(Logo);
         System.out.println("What I can do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if ("bye".equals(input.trim())) {
+                System.out.println(LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(LINE);
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+
+        sc.close();
     }
 }
