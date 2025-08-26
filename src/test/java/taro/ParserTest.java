0964e6d6130c9d54b19b90950895bf889ff5ef81
stefuiii <jianglh0425@outlook.com>
2025-08-27 00:16:57 +0800

    Add JUnit test script

---
+++ src/test/java/taro/ParserTest.java
@@ -0,0 +1,57 @@
+package taro;
+
+import org.junit.jupiter.api.Test;
+import taro.task.Task;
+import taro.task.Todo;
+import taro.task.Deadline;
+import taro.command.Command;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+    static class TestUi extends Ui {
+        private final StringBuilder buf = new StringBuilder();
+        @Override public void show(String msg) {
+            buf.append(msg).append('\n');
+        }
+        @Override public void showLine() {
+            buf.append("LINE\n");
+        }
+
+    }
+
+
+    static class TestStorage extends Storage {
+        TestStorage() {
+            super("teststore.txt");
+        }
+        @Override
+        public void save(TaskList tasks) { }
+    }
+
+
+    @Test
+    public void parseTask_deadline_load_from_file_correct() {
+        String line = "D | 0 | return book | 2025-08-30";
+        Task t = Parser.parseTask(line);
+        assertTrue(t instanceof Deadline);
+        String s = t.toString();
+        assertTrue(s.contains("return book"));
+        assertTrue(s.contains("2025"));
+    }
+
+    @Test
+    public void parseCommand_todo_generatesCommand() throws Exception {
+        Command c = Parser.parseCommand("todo read book");
+        TestUi ui = new TestUi();
+        TestStorage storage = new TestStorage();
+        TaskList tasks = new TaskList();
+
+        c.execute(tasks, ui, storage);
+        assertEquals(1, tasks.size());
+        assertTrue(tasks.get(0) instanceof Todo);
+        assertEquals("[T][ ] read book", tasks.get(0).toString());
+    }
+
+}
---
+++ src/test/java/taro/task/TaskTest.java
@@ -0,0 +1,24 @@
+package taro.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void markAsDone_and_toString_reflectsChange() {
+        Task t = new Task("read book", false);
+        assertEquals("[ ] read book", t.toString());
+        t.markAsDone();
+        assertEquals("[X] read book", t.toString()); // markAsDone -> X
+    }
+
+    @Test
+    public void toFile_formatsCorrectly_forDoneAndUndone() {
+        Task t1 = new Task("return book", false);
+        assertEquals("| 0 | return book", t1.toFile());
+
+        Task t2 = new Task("return book", true);
+        assertEquals("| 1 | return book", t2.toFile());
+    }
+}
